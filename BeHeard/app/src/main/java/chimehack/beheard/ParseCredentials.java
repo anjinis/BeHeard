1f6884ac228a7a9384cce407244c81e9aff26685
Kevin Hou <kevin.hou@sjsu.edu>
2015-07-11 09:55:16 -0700

    temp added ParseCredentials.java

---
+++ BeHeard/app/src/main/java/chimehack/beheard/ParseCredentials.java
@@ -0,0 +1,17 @@
+package chimehack.beheard;
+
+/**
+ * Created by kevin on 7/11/15.
+ */
+public class ParseCredentials {
+    private String API_KEY = "SET KEY HERE";
+    private String CLIENT_KEY = "SET CLIENT KEY HERE";
+
+    public String getAPI_KEY() {
+        return API_KEY;
+    }
+
+    public String getCLIENT_KEY() {
+        return CLIENT_KEY;
+    }
+}
